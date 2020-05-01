250f230f290a7e2b5deadc0ecd31c0a6340dc3f4
John Track <54856993+jltrack@users.noreply.github.com>
2020-04-30 20:07:56 -0400

    Create 5.1.js

---
+++ 5.1.js
@@ -0,0 +1,11 @@
+let price = 12;
+    taxRate = 0.07;
+
+
+showMessage(price--);
+console.log(price);
+
+
+/* 
+putting -- after price does the operation after. showMessage(price--); gets you 12 on website 11 on log
+putting -- before price does the operation before. showMessage(--price); gets you 11 on website 11 on log
