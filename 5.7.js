553857e9d940ff821ddb8e6ddc6a54dce2de2551
John Track <54856993+jltrack@users.noreply.github.com>
2020-05-09 14:49:25 -0400

    Create 5.7.js

---
+++ 5.7.js
@@ -0,0 +1,17 @@
+let message = 123; 
+// message = message.length;
+showMessage(message + 2);
+
+/*
+let message = 'Hello'; \nmessage = message.toLowerCase();\nshowMessage(message);
+let message = 'Hello'; \nmessage = message.toUpperCase();\nshowMessage(message);
+let message = 'Hello'; \nmessage = message.substring(1);\nshowMessage(message);
+let message = 'Hello'; \nmessage = message.substring(1);\nshowMessage(message);
+let message = 'Hello'; \nmessage = message.length;\nshowMessage(message);
+let message = 'Hello'; \nmessage = message.length;\nshowMessage(typeof message);
+let message = 'Hello'; \n// message = message.length;\nshowMessage(typeof message);
+let message = '123'; \n// message = message.length;\nshowMessage(typeof message);
+with quotes is a string and without quotes is a number
+let message = '123'; \n// message = message.length;\nshowMessage(message + 2);\n\n//prints 1232 i.e. it adds a string to 123 which is 1232
+let message = 123; \n// message = message.length;\nshowMessage(message + 2);\n\n//prints 125
+*/
