d14a2ad69c1284f58f71409c50a149522bb33761
John Track <54856993+jltrack@users.noreply.github.com>
2020-05-09 16:46:14 -0400

    Create 5.11.js

---
+++ 5.11.js
@@ -0,0 +1,22 @@
+// Objects and Symbols
+
+//Objects are foundational
+//In an Object, set properties inside an object. Properties are similar to variables
+//Symbols are advanced
+
+let person = {
+    firstName: ‘John’,
+    lastName: ‘Adams’
+};
+showMessage(typeof person);
+
+//object is printed
+
+let person = {
+    firstName: ‘John’,
+    lastName: ‘Adams’
+};
+showMessage(person.firstName);
+
+//John is printed
+//The dot operator takes an object and pulls up one of the properties
