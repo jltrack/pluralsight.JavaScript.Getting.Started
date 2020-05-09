0d48d336ec2c39faa32d3c2f9e99c80d7335e92d
John Track <54856993+jltrack@users.noreply.github.com>
2020-05-09 16:07:25 -0400

    Create 5.9.js

---
+++ 5.9.js
@@ -0,0 +1,25 @@
+// Boolean Variables
+// True or False
+let saved = true;
+saved = !saved;
+showMessage(saved);
+
+/* trying notes in Bear instead of PluralSight notes
+let saved = false;
+showMessage(typeof saved);
+// prints boolean
+
+let saved = false;
+showMessage(saved);
+// prints false
+
+let saved = true;
+showMessage(saved);
+//prints true
+
+let saved = true;
+saved = !saved;
+showMessage(saved);
+
+//flips it, prints false because it is true and it prints false because it is ! which is not
+*/
