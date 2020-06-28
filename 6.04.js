da17603be580bd27efdf7a62d7a93d2742aa599b
John Track <54856993+jltrack@users.noreply.github.com>
2020-06-28 17:19:28 -0400

    Create 6.04.js

---
+++ 6.04.js
@@ -0,0 +1,14 @@
+if (true) {
+    showMessage('true');
+}
+else {
+    showMessage('false;');
+}
+
+let price = 4;
+if (price > 10) {
+    showMessage('true');
+}
+else if (price < 5) {
+    showMessage('less than 5');
+}
