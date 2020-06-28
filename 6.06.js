88f4206ac79714475e367729b4a44d479b310f09
John Track <54856993+jltrack@users.noreply.github.com>
2020-06-28 18:32:12 -0400

    Create 6.06.js

---
+++ 6.06.js
@@ -0,0 +1,17 @@
+/*
+
+(condition) ? true-statement : false-statement;
+
+e.g.
+
+let message = (price > 10) ? 'expensive' : 'cheap';
+
+showMessage(message); //expensive 
+
+*/
+
+let price = 20;
+
+(price > 10) ? showMessage('yes') : showMessage('no');
+
+// yes shows up. If you switch > to < then it changes to no
