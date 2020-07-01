d16e8adabb6c0437ae34ae3c0301a274c9c6ff3e
John Track <54856993+jltrack@users.noreply.github.com>
2020-06-30 20:50:40 -0400

    Create 6.10.js

---
+++ 6.10.js
@@ -0,0 +1,20 @@
+ 
+/*
+let count = 1
+do {
+    console.log(count);
+    count++
+} while (count < 5);
+// 1 2 3 4
+
+Use do while loops when it's guaranteed to run at least once
+
+*/
+
+let i = -4
+do {
+    console.log(i)
+    i--;
+} while (i > 0);
+
+// prints -4 in console because the condition isn't met but it's a do while loop so it ran once
